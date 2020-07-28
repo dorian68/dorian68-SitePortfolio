6813474f90ce3193a02f7e589ff4c309e3144265
dorian68 <labrydorian.mpsi@hotmail.fr>
2020-07-28 18:03:31 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">    
+      <link href="/assets/css/material-kit.css?v=2.0.4" rel="stylesheet" />
+  
+        <p>
+          Website in construction
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <body>
+        <a>Salut Dorian</a>
+
+ <script src="assets/js/core/jquery.min.js" type="text/javascript"></script>
+<script src="assets/js/core/popper.min.js" type="text/javascript"></script>
+<script src="assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
+<script src="assets/js/plugins/moment.min.js"></script>
+
+<script src="assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
+
+<script src="assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+
+<script src="assets/js/material-kit.js?v=2.0.4" type="text/javascript"></script>
+      </body>
+    </div>
+  );
+}
+
+export default App;
